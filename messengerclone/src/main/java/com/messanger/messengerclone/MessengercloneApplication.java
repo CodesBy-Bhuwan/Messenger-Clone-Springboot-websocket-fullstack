18227554e1eba14e972d471eff1020627515870d
CodesBy-Bhuwan <bhuwan.thadarai817@gmail.com>
2024-03-15 22:53:59 +0545

    startingMessengerCloneApp

---
+++ messengerclone/src/main/java/com/messanger/messengerclone/MessengercloneApplication.java
@@ -0,0 +1,13 @@
+package com.messanger.messengerclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengercloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengercloneApplication.class, args);
+	}
+
+}
---
+++ messengerclone/src/test/java/com/messanger/messengerclone/MessengercloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messanger.messengerclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengercloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
