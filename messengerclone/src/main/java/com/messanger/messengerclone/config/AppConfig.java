651962c6633ebb07f2eec8095a7e7e59bffbfeb5
CodesBy-Bhuwan <bhuwan.thadarai817@gmail.com>
2024-03-20 23:29:39 +0545

    sec&&Auth utensils

--- messengerclone/src/main/java/com/messanger/messengerclone/config/AppConfig.java
+++ messengerclone/src/main/java/com/messanger/messengerclone/config/AppConfig.java
@@ -1,15 +1,41 @@
 package com.messanger.messengerclone.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class AppConfig {
 
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().authorizeHttpRequests(authorize->authorize.requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll()
+                ).addFilterBefore(null, null).csrf().disable()
+                .cors().configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration cfg=new CorsConfiguration();
+                        cfg.setAllowedOrigins(Arrays.asList(
+//                        "urlOfFontEnd"
+                        ));
+                        cfg.setAllowedMethods(Collections.singletonList("*"));
+                        
+
+
+                        return null;
+                    }
+                })
+                .and().formLogin().and().httpBasic();
         return http.build();
     }
     
