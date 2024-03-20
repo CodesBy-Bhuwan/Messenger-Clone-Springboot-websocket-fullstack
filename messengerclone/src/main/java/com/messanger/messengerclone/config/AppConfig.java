40be60562aa4a3509126f923d5fd60faa8b87801
CodesBy-Bhuwan <bhuwan.thadarai817@gmail.com>
2024-03-20 23:07:02 +0545

    init_spring sec

--- messengerclone/src/main/java/com/messanger/messengerclone/config/AppConfig.java
+++ messengerclone/src/main/java/com/messanger/messengerclone/config/AppConfig.java
@@ -1,9 +1,16 @@
 package com.messanger.messengerclone.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class AppConfig {
 
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        
+        return http.build();
+    }
     
 }
