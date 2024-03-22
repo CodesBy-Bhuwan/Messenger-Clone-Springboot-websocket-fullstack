36538b37ccb13c371ba40628d6c2f6041d915a6b
CodesBy-Bhuwan <bhuwan.thadarai817@gmail.com>
2024-03-22 23:34:17 +0545

    Override-Filter

--- messengerclone/src/main/java/com/messanger/messengerclone/config/JwtTokenValidator.java
+++ messengerclone/src/main/java/com/messanger/messengerclone/config/JwtTokenValidator.java
@@ -1,8 +1,23 @@
 package com.messanger.messengerclone.config;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-public class JwtTokenValidator extends OncePerRequestFilter {
+import java.io.IOException;
 
-    
+public class JwtTokenValidator extends OncePerRequestFilter{
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String jwt=request.getHeader("Authorization");
+
+    }
 }
+
