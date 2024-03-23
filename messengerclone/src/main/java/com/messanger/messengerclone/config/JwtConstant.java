3fe2817d2c3e4df9750452bbc7490f94c18e3cc7
CodesBy-Bhuwan <bhuwan.thadarai817@gmail.com>
2024-03-23 23:09:20 +0545

    JwtConstants

---
+++ messengerclone/src/main/java/com/messanger/messengerclone/config/JwtConstant.java
@@ -0,0 +1,10 @@
+package com.messanger.messengerclone.config;
+
+import org.springframework.security.web.header.Header;
+
+public class JwtConstant {
+
+    public static final String JWT_Header="Authorization";
+    public static final String SECRET_KEY="ow9itklnlmcnvoiszudfjkerwnicoanhsrutjnasdlkjcnaisruhtg";
+    
+}
